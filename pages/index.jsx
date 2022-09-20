baf0bac3a075cb7531273fb9c288e0535a41352c
Tom Harris <tomharris3@me.com>
2022-09-20 17:04:40 +0100

    create header component

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,8 @@
 import Head from 'next/head';
+import Header from '../components/Header';
+import TempoDisplay from '../components/TempoDisplay';
 
-export default function Home () {
+export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
@@ -9,7 +11,8 @@ export default function Home () {
       </Head>
 
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">Welcome to Tempo Toolbox!</h1>
+        <Header />
+        <TempoDisplay tempo={50} />
       </main>
 
       <footer className="flex h-24 w-full items-center justify-center border-t">
@@ -17,5 +20,4 @@ export default function Home () {
       </footer>
     </div>
   );
-};
-
+}
