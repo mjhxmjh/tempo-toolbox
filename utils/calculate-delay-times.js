4ad2d1705bac29c9e807265982eb9c180c7fc591
Tom Harris <tomharris3@me.com>
2022-09-22 13:44:04 +0100

    describe what the function does

--- utils/calculate-delay-times.js
+++ utils/calculate-delay-times.js
@@ -1,3 +1,25 @@
+// This function calculates the length of note values (in milliseconds) based on the bpm.
+// It takes two inputs: note values and bpm
+
+// In music, note values are the lengths of musical notes
+// e.g. crotchet = 1/4, quaver = 1/8
+
+// THINGS TO CONSIDER
+// Should the 'noteValues' be a string or number?
+// e.g. ['1/4', '1/8'], or [0.25, 0.125]
+
+// what value are the milliseconds rounded to?
+// This should be stored as variable
+
+// if the notevalues array does not exist, should it default to straight?
+
+// EXAMPLE OF WORKING OUT TRIPLETS
+// 1/16t = a eigth note divded by 3
+// 1/8t = a quarter note divided by 3
+// 1/4t = a half note divded by 3
+
+// How to work out dotted...?
+
 function calculateDelayTimes() {}
 
 module.exports = calculateDelayTimes;
