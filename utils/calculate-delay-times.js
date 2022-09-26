448b3561beb1b9aea555f6c59d6c739095e755da
Tom Harris <tomharris3@me.com>
2022-09-26 15:20:37 +0100

    add markdown file with extra info

--- utils/calculate-delay-times.js
+++ utils/calculate-delay-times.js
@@ -1,31 +1,30 @@
-// This function calculates the length of note values (in milliseconds) based on the bpm.
-// It takes two inputs: note values and bpm
+// This function calculates the length of note values (in milliseconds) and frequency (in Hz) based on the bpm.
+// It takes two inputs: note values array and bpm
 
-// In music, note values are the lengths of musical notes
-// e.g. crotchet = 1/4, quaver = 1/8
+// return a dataset that looks like:
 
-// EXAMPLE OF WORKING OUT TRIPLETS
-// triplets are notes grouped into 3
-// 1/16t = a eigth note divded by 3
-// 1/8t = a quarter note divided by 3
-// 1/4t = a half note divded by 3
+/* 
+const delayValues = [
+  {
+    noteValue: '1/8',
+    duration: 2400.00,
+    frequency: 0.1,
 
-// With this in mind, work out half notes (1/2) first!
+  },
+  {
+    noteValue: '1/4t',
+    duration: 2400,
+    frequency: 0.4,
+  },
+  {
+    noteValue: '1/16',
+    duration: 400,
+    frequency: 0.2,
+  },
+];
 
-// WORKING OUT DOTTED NOTES
-// dotted notes are the value plus half (x1.5)
-// 1/8d = a semiquaver note times by 3
-// 1/4d = a quaver note times by 3
-// 1/2d = a crotchet note times b
 
-// THINGS TO CONSIDER
-// Should the 'noteValues' be a string or number?
-// e.g. ['1/4', '1/8'], or [0.25, 0.125]
-
-// what value are the milliseconds rounded to?
-// This should be stored as variable
-
-// if the notevalues array does not exist, should it default to straight?
+*/
 
 function calculateDelayTimes() {}
 
--- utils/calculate-duration-times.md
+++ utils/calculate-duration-times.md
@@ -1,4 +1,3 @@
-const calculateDelayTimes = require('../calculate-delay-times');
 // TEST IDEAS
 // function returns an array of objects
 // function returns delay times fixed to nearest decimal place e.g. 312.518 => 312.52
@@ -29,3 +28,38 @@ const calculateDelayTimes = require('../calculate-delay-times');
 // EDGE CASES
 // bpm over 999 is not allowed
 // output array length is the same as input
+
+
+
+
+
+___________
+
+
+
+
+In music, note values are the lengths of musical notes
+e.g. crotchet = 1/4, quaver = 1/8
+
+EXAMPLE OF WORKING OUT TRIPLETS
+triplets are notes grouped into 3
+1/16t = a eigth note divded by 3
+1/8t = a quarter note divided by 3
+1/4t = a half note divded by 3
+
+With this in mind, work out half notes (1/2) first!
+
+WORKING OUT DOTTED NOTES
+dotted notes are the value plus half (x1.5)
+1/8d = a semiquaver note times by 3
+1/4d = a quaver note times by 3
+1/2d = a crotchet note times b
+
+THINGS TO CONSIDER
+Should the 'noteValues' be a string or number?
+e.g. ['1/4', '1/8'], or [0.25, 0.125]
+
+what value are the milliseconds rounded to?
+This should be stored as variable
+
+if the notevalues array does not exist, should it default to straight?
