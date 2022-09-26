ece91f1ba6b736568fcbca63772197b54a701d41
Tom Harris <tomharris3@me.com>
2022-09-26 18:17:22 +0100

    function returns individual values, simple, dotted and triplets

--- utils/calculate-delay-times.js
+++ utils/calculate-delay-times.js
@@ -1,8 +1,13 @@
-function calculateDelayTimes(bpm) {
-  const floatSum = (60 / bpm) * 1000;
-  return Number(floatSum.toFixed(2));
+// 1/1 = semibreve, whole = 4
+// 1/2 = minum, half = 2
+// 1/4 = crotchet, quarter = 1
+// 1/8 = quaver, eigth = 0.5
+// 1/16 = semiquaver, sixteenth = 0.25
+// 1/32 = demisemiquaver, thirtysecond = 0.125
+
+function calculateDelayTimes(bpm, numOfBeats = 1) {
+  const lengthOfNote = (60 / bpm) * 1000;
+  return Number((lengthOfNote * numOfBeats).toFixed(2));
 }
 
 module.exports = calculateDelayTimes;
-
-// const rounded = Number((6.6756854).toFixed(1));
