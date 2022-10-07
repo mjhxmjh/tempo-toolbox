9cec89d0da2fa97f424876e00259a215117b4e1f
Tom Harris <tomharris3@me.com>
2022-10-07 17:47:28 +0100

    switch cases works with non-trpliet notes

--- utils/note-values-to-text/note-values-to-text.js
+++ utils/note-values-to-text/note-values-to-text.js
@@ -1,57 +1,59 @@
 // this function accepts an array of note values in number form
 // it returns an array of note values converted into text
-/*
-    input: 
-
-    note values
-
-    noteValues = [2, 1, 0.5, 0.25, 0.125]
-    [1/2, 1/4, 1/8, 1/16, 1/32]
-
-    noteValues = [3, 1.5, 0.75]
-    ['1/2d', '1/4d', '1/8d']
-
-    noteValues = [0.66666666666, 0.33333333333, 0.16666666666]
-
-    noteValues = [4, 3, 2, 1.5, 1, 0.66666666666, 0.75, 0.5, 0.33333333333, 0.25, 0.16666666666 ]
-    
-
-    note values to text
-    [1 bar, 1/2d, 1/2, 1/4d, 1/4, 1/4t, 1/8d, 1/8, 1/8t, 1/16d, 1/16, 1/16t, 1/32]
-
-*/
 
 const noteValuesToText = (array) => {
   const outputArray = [];
+  const regex =
+    /(?<triplet4>\d\.6+\d*):(?<triplet8>\d\.3+\d*):(?<triplet16>\d\.16+\d*)/;
 
-  array.forEach((note) => {
+  const matchObj = regex.exec(array.join(':'));
+
+  array.forEach((note, i) => {
+    if (matchObj) {
+      //   console.log('yes');
+      switch (note) {
+        case parseFloat(matchObj.groups.triplet4):
+          outputArray[i] = '1/4t';
+          break;
+        case parseFloat(matchObj.groups.triplet8):
+          outputArray[i] = '1/8t';
+          break;
+        case parseFloat(matchObj.groups.triplet16):
+          outputArray[i] = '1/16t';
+          break;
+      }
+    }
+
     switch (note) {
       case 4:
-        outputArray.push('1 bar');
+        outputArray[i] = '1 bar';
         break;
       case 3:
-        outputArray.push('1/2d');
+        outputArray[i] = '1/2d';
         break;
       case 2:
-        outputArray.push('1/2');
+        outputArray[i] = '1/2';
         break;
       case 1.5:
-        outputArray.push('1/4d');
+        outputArray[i] = '1/4d';
         break;
       case 1:
-        outputArray.push('1/4');
+        outputArray[i] = '1/4';
         break;
       case 0.75:
-        outputArray.push('1/8d');
+        outputArray[i] = '1/8d';
         break;
       case 0.5:
-        outputArray.push('1/8');
+        outputArray[i] = '1/8';
+        break;
+      case 0.375:
+        outputArray[i] = '1/16d';
         break;
       case 0.25:
-        outputArray.push('1/16');
+        outputArray[i] = '1/16';
         break;
       case 0.125:
-        outputArray.push('1/32');
+        outputArray[i] = '1/32';
         break;
     }
   });
