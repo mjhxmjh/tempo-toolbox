9180d446f6e8d83795d053387d1838f25d230ffc
Tom Harris <tomharris3@me.com>
2022-10-07 14:59:08 +0100

    function converts single 1 beat

--- utils/__tests__/note-values-to-text.test.js
+++ utils/__tests__/note-values-to-text.test.js
@@ -1,9 +1,18 @@
-const { isExportDeclaration } = require('typescript');
-const noteValuesToText = require('../note-values-to-text.js');
+const noteValuesToText = require('../note-values-to-text/note-values-to-text');
 
 describe('note values to text', () => {
   test('should return an array', () => {
     const input = [];
     expect(Array.isArray(noteValuesToText(input))).toBe(true);
+  });
+  test('should convert one basic element', () => {
+    const input = [1];
+    expect(noteValuesToText(input)).toEqual(['1/4']);
+  });
+  test('should convert multiple values to text', () => {
+    const input = [2, 1, 0.5, 0.25, 0.125];
+    const convertedValues = ['1/2', '1/4', '1/8', '1/16', '1/32'];
+
+    expect(noteValuesToText(input)).toEqual(convertedValues);
   });
 });
--- utils/note-values-to-text/note-values-to-text.js
+++ utils/note-values-to-text/note-values-to-text.js
@@ -1,13 +1,36 @@
 // this function accepts an array of note values in number form
-// it returns an array of ntoe values converted into text
-// text should be in the format of
+// it returns an array of note values converted into text
 /*
     input: 
-    [4, 3, 2, 1.5, 1, (2/3), 0.75, 0.5, (1/3), 0.25, ]
+
+    note values
+
+    noteValues = [2, 1, 0.5, 0.25, 0.125]
+    [1/2, 1/4, 1/8, 1/16, 1/32]
+
+    noteValues = [3, 1.5, 0.75]
+    noteValues = [0.66666666666, 0.33333333333, 0.16666666666]
+
+    noteValues = [4, 3, 2, 1.5, 1, 0.66666666666, 0.75, 0.5, 0.33333333333, 0.25, 0.16666666666 ]
+    
+
+    note values to text
+    [1 bar, 1/2d, 1/2, 1/4d, 1/4, 1/4t, 1/8d, 1/8, 1/8t, 1/16d, 1/16, 1/16t, 1/32]
+
 */
-// [1 bar, 1/2d, 1/2, 1/4d, 1/4, 1/4t, 1/8d, 1/8, 1/8t, 1/16d, 1/16, 1/16t, 1/32]
 
+const noteValuesToText = (array) => {
+  const outputArray = [];
 
-const noteValuesToText = () => {
+  
 
-}
+  switch (array[0]) {
+    case 1:
+      outputArray.push('1/4');
+      break;
+  }
+
+  return outputArray;
+};
+
+module.exports = noteValuesToText;
