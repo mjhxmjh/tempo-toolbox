bee5aa3b43068dd02a3aae3e7ff7ab53978a22f0
Tom Harris <tomharris3@me.com>
2022-10-10 11:38:40 +0100

    add test for simple function

--- utils/__tests__/copy-array.test.js
+++ utils/__tests__/copy-array.test.js
@@ -4,4 +4,10 @@ describe('Copy Array', () => {
   test('should return an empty array given empty array input', () => {
     expect(copyArray([])).toEqual([]);
   });
+  test('can run a simple function to multiple elements', () => {
+    const array = [2, 10, 55];
+    const simpleFunc = (num) => num * 2;
+    const newArray = [4, 20, 110];
+    expect(copyArray(array, simpleFunc)).toEqual(newArray);
+  });
 });
