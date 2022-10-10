e9f182589cf2e107f6e35b23fed5af7a57b9f4c0
Tom Harris <tomharris3@me.com>
2022-10-10 12:04:45 +0100

    test for triplet values

--- utils/beat-value-to-note/beat-value-to-note.js
+++ utils/beat-value-to-note/beat-value-to-note.js
@@ -1,6 +1,8 @@
 function beatValueToNote(n) {
   const simpleBeatValues = [4, 2, 1, 0.5, 0.25, 0.125];
   const dottedBeatValues = [3, 1.5, 0.75, 0.375];
+  const isTriplet = /\d\.6+\d*|\d\.3+\d*|\d\.16+\d*/.test(n.toString());
+
   if (simpleBeatValues.includes(n)) {
     const noteValue = 1 / n / 0.25;
     return `1/${noteValue}`;
@@ -8,6 +10,10 @@ function beatValueToNote(n) {
   if (dottedBeatValues.includes(n)) {
     const noteValue = Math.round(1 / n / 0.1666666666);
     return `1/${noteValue}d`;
+  }
+  if (isTriplet) {
+    const formula = Math.round(1 / n / 0.375);
+    return `1/${formula}t`;
   }
 }
 
