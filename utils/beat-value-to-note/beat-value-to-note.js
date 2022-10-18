9c8800b2c8964b3346c126dab91f54cba882e84c
Matt J Hampson <mjhampson@gmail.com>
2022-10-18 17:35:40 +0100

    small refactor of function

--- utils/beat-value-to-note/beat-value-to-note.js
+++ utils/beat-value-to-note/beat-value-to-note.js
@@ -1,5 +1,5 @@
 function beatValueToNote(n) {
-  if (n === 4) return '1 bar';
+  if (n === 4) return "1 bar";
   const simpleBeatValues = [4, 2, 1, 0.5, 0.25, 0.125];
   const dottedBeatValues = [3, 1.5, 0.75, 0.375];
   const isTriplet = /\d\.6+\d*|\d\.3+\d*|\d\.16+\d*/.test(n.toString());
@@ -13,8 +13,8 @@ function beatValueToNote(n) {
     return `1/${noteValue}d`;
   }
   if (isTriplet) {
-    const formula = Math.round(1 / n / 0.375);
-    return `1/${formula}t`;
+    const noteValue = Math.round(1 / n / 0.375);
+    return `1/${noteValue}t`;
   }
 }
 
