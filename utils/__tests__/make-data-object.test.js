d09ac10ec2fa67616934a33816080a5b0b3890b6
Tom Harris <tomharris3@me.com>
2022-10-19 18:14:30 +0100

    add initial working test for using the utility functions

--- utils/__tests__/make-data-object.test.js
+++ utils/__tests__/make-data-object.test.js
@@ -1,6 +1,9 @@
 const { executionAsyncId } = require('async_hooks');
 const makeDataObject = require('../make-data-object/make-data-object');
-// const beatValueToNote = require('../beat-value-to-note/beat-value-to-note');
+const beatValueToNote = require('../beat-value-to-note/beat-value-to-note');
+const calculateDelayTimes = require('../calculate-delay-times/calculate-delay-times');
+const copyArray = require('../copy-array/copy-array');
+const { data } = require('autoprefixer');
 
 describe('function should return an array', () => {
   test('when given an empty input, function returns an empty array', () => {
@@ -32,5 +35,39 @@ describe('function should return an array', () => {
     expect(makeDataObject(initialKeys, noteValues, durations)).toMatchObject(
       dataObject
     );
+  });
+
+  describe('Using the utility functions', () => {
+    // Remind ourselves of the flow, check the "function-flow-diagram"
+
+    test('should check for matched properties', () => {
+      // 1. Take the beat values and convert them to note values
+      const beatValues = [4, 2, 1, 0.5, 0.25, 0.125];
+      const noteValues = Array.from(beatValues, beatValueToNote);
+      // console.log(beatValues, noteValues);
+
+      // 2. Take the beat values and BPM, convert them into delay times (i.e. duration)
+      const bpm = 120;
+      const delayTimesInMs = calculateDelayTimes(bpm, beatValues);
+      // console.log(noteValues, delayTimesInMs);
+
+      // 3. Build data structure with the two
+      const dataObject = makeDataObject(
+        ['note_values', 'duration'],
+        noteValues,
+        delayTimesInMs
+      );
+      // console.log(dataObject);
+
+      // 4. Test that each object in the array contains the correct information
+      dataObject.forEach((object) => {
+        expect(object).toEqual(
+          expect.objectContaining({
+            note_values: expect.any(String),
+            duration: expect.any(Number),
+          })
+        );
+      });
+    });
   });
 });
