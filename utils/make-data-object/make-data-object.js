797fb2897eeaee8f938099a54087d4e50fd8452f
Tom Harris <tomharris3@me.com>
2022-10-19 17:51:41 +0100

    use the values for loop to create durations key-value pair

--- utils/make-data-object/make-data-object.js
+++ utils/make-data-object/make-data-object.js
@@ -3,30 +3,14 @@ function makeDataObject(keys, values, durations) {
     return [];
   }
   const arr = [];
+
   for (let i = 0; i < values.length; i++) {
     const dataObject = {};
     dataObject[keys[0]] = values[i];
-    console.log(durations[i]);
+    if (durations) dataObject[keys[1]] = durations[i];
     arr.push(dataObject);
   }
   return arr;
 }
 
 module.exports = makeDataObject;
-
-// returned ouput should look something like this:
-
-// delayValues = [
-//     {
-//         noteValue: '1/8',
-//         duration: 2400.00,
-//     },
-//     {
-//         noteValue: '1/4t',
-//         duration: 2400,
-//     },
-//     {
-//         noteValue: '1/16',
-//         duration: 400,
-//     },
-// ];
